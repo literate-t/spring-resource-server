fcc6883802db87cc71466c386d29871bf39fb3e6
literate <gaekoon@gmail.com>
2024-02-12 02:10:20 +0900

    Initial commit

---
+++ src/test/java/io/security/springresourceserver/SpringResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResourceServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
