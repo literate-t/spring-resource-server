2e9e192b970640c891456bb615e38b974e4024fd
literate <gaekoon@gmail.com>
2024-02-14 14:32:13 +0900

    Feat: 토큰에 서명하는 최상위 클래스
    - getJwtToken는 구현체에서 구현된다

--- src/main/java/io/security/springresourceserver/signature/SecuritySigner.java
+++ src/main/java/io/security/springresourceserver/signature/SecuritySigner.java
@@ -1,5 +1,39 @@
 package io.security.springresourceserver.signature;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public abstract class SecuritySigner {
 
+  public abstract String getJwtToken(UserDetails user, JWK jwk) throws JOSEException;
+
+  protected String getJwtTokenInternal(MACSigner jwsSigner, UserDetails user, JWK jwk)
+      throws JOSEException {
+    JWSHeader header = new JWSHeader.Builder((JWSAlgorithm) jwk.getAlgorithm()).keyID(
+        jwk.getKeyID()).build();
+
+    List<String> authorities = user.getAuthorities().stream().map(auth -> auth.getAuthority())
+        .collect(Collectors.toList());
+    JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+        .subject("user")
+        .issuer("http://localhost:8081")
+        .claim("username", user.getUsername())
+        .claim("authority", authorities)
+        .expirationTime(new Date(new Date().getTime() + 1000 * 60 * 5)).build();
+
+    SignedJWT signedJWT = new SignedJWT(header, jwtClaimsSet);
+    signedJWT.sign(jwsSigner);
+    String jwtToken = signedJWT.serialize();
+
+    return jwtToken;
+  }
 }
