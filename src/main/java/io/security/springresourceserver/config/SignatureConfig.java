895d2c379f8b3e585e5e5c2294d0b4f19a0c60d7
literate <gaekoon@gmail.com>
2024-02-14 14:33:01 +0900

    Feat: MacSecuritySigner, OctetSequenceKey를 bean으로 등록

--- src/main/java/io/security/springresourceserver/config/SignatureConfig.java
+++ src/main/java/io/security/springresourceserver/config/SignatureConfig.java
@@ -1,9 +1,29 @@
 package io.security.springresourceserver.config;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
+import io.security.springresourceserver.signature.MacSecuritySigner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // for sign and verification
 @Configuration
 public class SignatureConfig {
 
+  @Bean
+  public MacSecuritySigner macSecuritySigner() {
+    return new MacSecuritySigner();
+  }
+
+  @Bean
+  public OctetSequenceKey octetSequenceKey() throws JOSEException {
+    OctetSequenceKey octetSequenceKey = new OctetSequenceKeyGenerator(256)
+        .keyID("mackey")
+        .algorithm(JWSAlgorithm.HS256)
+        .generate();
+
+    return octetSequenceKey;
+  }
 }
