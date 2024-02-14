7fba47050113383fbe3a53f6a516a7c1c6cd662a
literate <gaekoon@gmail.com>
2024-02-15 00:57:13 +0900

    Feat: RSA 방식의 필터 추가

---
+++ src/main/java/io/security/springresourceserver/filter/authorization/JwtAuthorizationRsaFilter.java
@@ -0,0 +1,10 @@
+package io.security.springresourceserver.filter.authorization;
+
+import com.nimbusds.jose.JWSVerifier;
+
+public class JwtAuthorizationRsaFilter extends JwtAuthorizationFilter {
+
+  public JwtAuthorizationRsaFilter(JWSVerifier jwsVerifier) {
+    super(jwsVerifier);
+  }
+}
