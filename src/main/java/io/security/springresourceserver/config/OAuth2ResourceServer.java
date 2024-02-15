f4b88143893ab92ffd0c88770b64a7388f425153
literate <gaekoon@gmail.com>
2024-02-15 20:46:23 +0900

    Feat: CustomOpaqueTokenIntrospector 생성 및 테스트

--- src/main/java/io/security/springresourceserver/config/OAuth2ResourceServer.java
+++ src/main/java/io/security/springresourceserver/config/OAuth2ResourceServer.java
@@ -1,21 +1,24 @@
 package io.security.springresourceserver.config;
 
+import io.security.springresourceserver.CustomOpaqueTokenIntrospector;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Opaquetoken;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OAuth2ResourceServer {
+
+  private final OAuth2ResourceServerProperties properties;
 
   @Bean
   public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
@@ -30,12 +33,18 @@ public class OAuth2ResourceServer {
     return http.build();
   }
 
-  @Bean
-  public OpaqueTokenIntrospector opaqueTokenIntrospector(
-      OAuth2ResourceServerProperties properties) {
-    Opaquetoken opaquetoken = properties.getOpaquetoken();
+//  @Bean
+//  public OpaqueTokenIntrospector opaqueTokenIntrospector(
+//      OAuth2ResourceServerProperties properties) {
+//    Opaquetoken opaquetoken = properties.getOpaquetoken();
+//
+//    return new NimbusOpaqueTokenIntrospector(opaquetoken.getIntrospectionUri(),
+//        opaquetoken.getClientId(), opaquetoken.getClientSecret());
+//  }
 
-    return new NimbusOpaqueTokenIntrospector(opaquetoken.getIntrospectionUri(),
-        opaquetoken.getClientId(), opaquetoken.getClientSecret());
+  @Bean
+  public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+    return new CustomOpaqueTokenIntrospector(properties);
   }
+
 }
